ba60f01bbfb4b1a48616463f61ccaf16ec2b7e6a
Wren <jenny@valvatne.com>
2023-01-23 13:45:39 +0100

    initial commit - crud framework set up

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,36 @@
+module.exports.descriptors = [
+    'Snowy',
+    'Windy',
+    'Rocky',
+    'Lonely',
+    'Sleepy',
+    'Painted',
+    'Crooked',
+    'Narrow',
+    'Dusty',
+    'Devils',
+    'Grizzly',
+    'Elk',
+    'Antelope',
+    'Bison'
+    
+]
+
+module.exports.places = [
+    'Cavern',
+    'Canyon',
+    'River',
+    'Woods',
+    'Forest',
+    'Gulch',
+    'Mesa',
+    'Ridge',
+    'Track',
+    'Mountain',
+    'Peak',
+    'Valley',
+    'Point',
+    'Lake',
+    'Pond',
+    'Trail'
+]
