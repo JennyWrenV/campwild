7eb25cafa19fb7a49ce683464c6c448fa60f3ae1
Wren <jenny@valvatne.com>
2023-02-08 14:19:06 +0100

    adds flash and session

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose');
-const Campground = require('./models/campground')
-const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
-const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
-const { campgroundSchema, reviewSchema } = require('./schemas.js')
-const Review = require('./models/review')
-const campgrounds = require('./routes/campgrounds')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
 
+const campgrounds = require('./routes/campgrounds')
+const reviews = require('./routes/reviews')
 
 
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, 
+useUnifiedTopology: true,
+
+ })
     .then(() => {
         console.log("MongoDatabase connection is open")
     })
@@ -22,12 +24,13 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true,
         console.log(err)
     })
 
-mongoose.set('strictQuery', false)
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected")
 });
+
 
 const app = express();
 
@@ -37,43 +40,38 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-
-
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
+const sessionConfig = {
+    secret: 'simplesecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
    
 }
+app.use(session(sessionConfig))
+app.use(flash())
+
+
+app.use((req, res, next) =>{
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
+})
+
 
 app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-
-})) 
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) =>{
-    const {id, reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page not found', 404));
