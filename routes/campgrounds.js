2dd354ab926e880a98ddb21fc70659d9aae33272
Wren <jenny@valvatne.com>
2023-02-15 15:00:20 +0100

    adds star form for ratings

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
-const Campground = require('../models/campground')
-const campgrounds = require('../controllers/campgrounds')
-const { isLoggedIn } = require('../middleware');
-const { isAuthor } = require('../middleware')
-const { validateCampground } = require('../middleware')
 
-router.get('/', catchAsync(campgrounds.index))
+const campgrounds = require('../controllers/campgrounds')
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+
+router.route('/')
+.get(catchAsync(campgrounds.index))
+.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+.get( catchAsync(campgrounds.showCampground))
+.put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editCampground))
+.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editCampground))
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
+
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,12 +1,8 @@
-
 const express = require('express')
 const router = express.Router({ mergeParams: true })
 const catchAsync = require('../utils/catchAsync')
-const Campground = require('../models/campground')
-const Review = require('../models/review')
 const reviews = require('../controllers/reviews')
 const {validateReview, isLoggedIn, isReviewAuthor} = require('../middleware')
-
 
 
 
